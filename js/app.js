497822ed5b381587b0da1fd522f04cb419ed820d
laura-gardner <elgardner@gmail.com>
2022-08-07 10:00:05 +0100

    hide joke container before first clicking button, change text in button to update after first clicking button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const jokePara = document.querySelector(".joke-paragraph");
 const jokeButton = document.querySelector(".joke-button");
+const jokeContainer = document.querySelector(".joke-container");
 
 jokeButton.addEventListener("click", function () {
     getRandomJoke();
@@ -10,4 +11,6 @@ const getRandomJoke = async () => {
     const res = await axios.get('https://icanhazdadjoke.com/', config);
     const joke = res.data.joke;
     jokePara.innerText = joke;
+    jokeContainer.classList.remove('hidden');
+    jokeButton.innerText = "Click for a new joke!";
 };
